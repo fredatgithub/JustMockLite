aa7f0bcf6653549269ff87b69172a80aaeaff34c
Kamen Ivanov <kamen.ivanov@telerik.com>
2017-06-27 18:38:08 +0300

    add tests for MSTestV2

---
+++ Telerik.JustMock.MS2.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Telerik.JustMock.MS2.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Telerik.JustMock.MS2.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b3f68a0c-4500-4334-b8c8-a9abc193bdda")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+#if LITE_EDITION
+[assembly: InternalsVisibleTo("Telerik.JustMock")]
+#endif
